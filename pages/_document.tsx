226e6a70a92bba8f238f8b69254ff2000326663f
denizarca <denizarca1@gmail.com>
2022-02-01 23:23:36 +0200

    seo fixes v2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,26 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+  	static async getInitialProps(ctx: DocumentContext) {
+    	const initialProps = await Document.getInitialProps(ctx)
+    	
+		return { ...initialProps }
   }
 
   render() {
     return (
       <Html lang='en'>
-          	<title>Deniz Arca&apos;s Web Presence</title>
+          	<title>{"Deniz Arca Web Presence"}</title>
         	<Head>
 				<meta charSet="utf-8" />
+				<link rel="canonical" href="https://denizarca.com" />
 				<link rel="shortcut icon" href="/public/favicon.ico" />
 				<link rel="shortcut icon" type="image/png" href="public/images/favicon.png"/>
 				<link rel="shortcut icon" sizes="192x192" href="public/images/favicon.ico"/>
 				<link rel="apple-touch-icon" href="/public/images/favicon.ico"/>
 
+				<meta name="author" content="Deniz Arca" />
+				<meta name="keywords" content="Software, Engineer, Developer, Full Stack, FullStack, Backend, Frontend, DevOps" /> 
     			<meta name="description" content="Personal portfolio site with an edge on reuse, targetting individuals who are in need of web presence. SEO Ready, Accessible & Extensible, based in React!"/>   
 			</Head>
         	<body>
